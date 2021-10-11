6ed12527792387108906938a784c1768581250b8
seubpong.mon <pjame.fb@gmail.com>
2021-10-11 14:09:34 +0700

    Fix use bas oauth2

--- yru2.py
+++ yru2.py
@@ -1,23 +1,40 @@
 """
-YRU OpenIdConnect
+Amazon OAuth2 backend, docs at:
+    https://python-social-auth.readthedocs.io/en/latest/backends/amazon.html
 """
+import ssl
 
-from .open_id_connect import OpenIdConnectAuth
+from .oauth import BaseOAuth2
 
-class YRUOpenIdConnect2(OpenIdConnectAuth):
+class YRUOpenIdConnect2(BaseOAuth2):
     name = 'yru-openidconnect2'
-    OIDC_ENDPOINT = 'https://passport.yru.ac.th/'
+    ID_KEY = 'user_id'
+    AUTHORIZATION_URL = 'https://passport.yru.ac.th/oauth/authorize'
+    ACCESS_TOKEN_URL = 'https://passport.yru.ac.th/oauth/token'
+    DEFAULT_SCOPE = ['openid', 'profile', 'email']
+    REDIRECT_STATE = True
+    ACCESS_TOKEN_METHOD = 'POST'
+
+    EXTRA_DATA = [
+        ('refresh_token', 'refresh_token', True)
+    ]
 
     def get_user_details(self, response):
-        return {'username': response.get('preferred_username'),
+        """Return user details from Yru account"""
+        return {'username': response.get('username'),
                 'email': response.get('email') or '',
-                'first_name': response.get('given_name'),
-                'last_name': response.get('family_name')}
+                'first_name': response.get('name'),
+                'last_name': ''}
+
 
     def user_data(self, access_token, *args, **kwargs):
-        return self.get_json(
-            'https://passport.yru.ac.th/api/identity/userinfo',
-            headers={
-                'Authorization': 'Bearer {}'.format(access_token),
-            }
-        )
+        response = self.get_json('https://passport.yru.ac.th/api/identity/userinfo',
+            headers={ 'Authorization': 'Bearer {}'.format(access_token) } )
+
+        response = {
+            'user_id': response['id'],
+            'name': response['name'],
+            'email': response['email']
+        }
+
+        return response
