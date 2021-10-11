585f81e72384554f50d9d0db96050ed6502655a9
doun_pct <doun_pct@hotmail.com>
2021-10-11 17:13:43 +0700

    test change to yru

---
+++ yru copy.py
@@ -0,0 +1,40 @@
+"""
+Amazon OAuth2 backend, docs at:
+    https://python-social-auth.readthedocs.io/en/latest/backends/amazon.html
+"""
+import ssl
+
+from .oauth import BaseOAuth2
+
+class YRUOpenIdConnect(BaseOAuth2):
+    name = 'yru-openidconnect'
+    ID_KEY = 'user_id'
+    AUTHORIZATION_URL = 'https://passport.yru.ac.th/oauth/authorize'
+    ACCESS_TOKEN_URL = 'https://passport.yru.ac.th/oauth/token'
+    DEFAULT_SCOPE = ['openid profile email']
+    REDIRECT_STATE = False
+    ACCESS_TOKEN_METHOD = 'POST'
+
+    EXTRA_DATA = [
+        ('refresh_token', 'refresh_token', True),
+        ('access_token', 'access_token', True)
+    ]
+
+    def get_user_details(self, response):
+        """Return user details from Yru account"""
+        return {'username': response.get('username'),
+                'email': response.get('email') or '',
+                'first_name': response.get('name'),
+                'last_name': ''}
+
+
+    def user_data(self, access_token, *args, **kwargs):
+        response = self.get_json('https://passport.yru.ac.th/api/identity/userinfo',
+                                 params={'access_token': access_token})
+        response = {
+            'user_id': response['id'],
+            'name': response['name'],
+            'email': response['email']
+        }
+
+        return response
