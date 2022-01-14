66c07fd450e63e6dc9b978d5a9a97f9c3247c1fb
seubpong.mon <pjame.fb@gmail.com>
2022-01-14 07:59:53 +0700

    Fixed template & refactored code

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
 FROM overhangio/openedx:12.2.0
 
-RUN pip install python-jose>=3.0.0
-
-COPY register.underscore /openedx/edx-platform/lms/templates/student_account/
-COPY _accomplishment-rendering.html /openedx/edx-platform/lms/templates/certificates
-COPY accomplishment-base.html /openedx/edx-platform/lms/templates/certificates
-
 COPY openedx-theme /openedx-theme
+COPY openedx-custom /openedx-custom
 
 COPY copy-files.bash copy-files.bash
 RUN ./copy-files.bash
--- copy-files.bash
+++ copy-files.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 SRC_DIR=/openedx-theme
+CUSTOM_DIR=/openedx-custom
 DEST_HTML=/openedx/edx-platform/lms/templates
 DEST_STATIC=/openedx/staticfiles
 
 find ${SRC_DIR}
 
+echo "#### Start copy files ###"
+
+cp ${CUSTOM_DIR}/register.underscore ${DEST_HTML}/student_account/
+cp ${CUSTOM_DIR}/_accomplishment-rendering.html ${DEST_HTML}/certificates/
+cp ${CUSTOM_DIR}/accomplishment-base.html ${DEST_HTML}/certificates/
+
 cp ${SRC_DIR}/css/* ${DEST_STATIC}/css/vendor/
 cp ${SRC_DIR}/images/* ${DEST_STATIC}/images/
 
-cp ${SRC_DIR}/html/* ${DEST_HTML}/
-cp ${SRC_DIR}/html/courseware/* ${DEST_HTML}/courseware/
-cp ${SRC_DIR}/html/discovery/* ${DEST_HTML}/discovery/
-cp ${SRC_DIR}/html/header/* ${DEST_HTML}/header/
-cp ${SRC_DIR}/html/dashboard/* ${DEST_HTML}/dashboard/
+cp -r ${SRC_DIR}/html/* ${DEST_HTML}/
+
+echo "#### List files dest dir [${DEST_HTML}] ###"
+find ${DEST_HTML}
